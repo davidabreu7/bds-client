2d852dd3a5f2665859219bdcd14e3db77349784d
David Abreu <david.abreu@mpdft.mp.br>
2022-05-29 16:47:09 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/bdsclient/BdsClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bdsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsClientApplication.class, args);
+	}
+
+}
